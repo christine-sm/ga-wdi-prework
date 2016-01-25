f3d2b03e53e672473c7f3ecd79814706bcf974de
Christine Movius <christine.movius@gmail.com>
2016-01-25 15:44:56 -0500

    created fizzbuzz loop file

---
+++ fizzbuzzloop.js
@@ -0,0 +1,15 @@
+var result;
+var max;
+
+var max = 20;
+for (var i = 1; i <=max; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+    console.log(result = 'fizzbuzz');
+} else if (i % 5 === 0) {
+    console.log(result = 'buzz');
+} else if (i % 3 === 0) {
+    console.log(result = 'fizz');
+} else {
+    console.log(result = i);
+}
+}
