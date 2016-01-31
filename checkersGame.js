b7b373333e6067af863153daf0c279e329246ea1
Christine Movius <christine.movius@gmail.com>
2016-01-31 12:26:27 -0500

    made instructions three shorter lines.

--- checkersGame.js
+++ checkersGame.js
@@ -13,7 +13,7 @@ function setSquare(player, row, col) {
 
 function getPieceAt(row, col) {
     var piece = checkerboard [row][col]; //could I replace checkerboard [row][col] with the setSquare function?
-    if (val == 'R' || val == 'B') {
+    if (piece === 'R' || piece === 'B') {
         return piece;
     } else {
         return null;
@@ -21,4 +21,5 @@ function getPieceAt(row, col) {
 }
 
 /*setSquare, which places a player (either 'R' or 'B') at a particular row and column on the board.
-getPieceAt, which returns the piece at a particular row and column; if there's no piece at that position, it should return null*/
+getPieceAt, which returns the piece at a particular row and column;
+if there's no piece at that position, it should return null*/
